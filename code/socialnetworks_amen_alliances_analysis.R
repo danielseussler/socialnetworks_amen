19cefe3f268eeb323d48a7b1899e50216869af66
danielseussler <daniel.seussler@gmail.com>
2021-04-28 19:13:02 +0200

    Start code for analysis

---
+++ code/socialnetworks_amen_alliances_analysis.R
@@ -0,0 +1,53 @@
+# Seminar on Statistical Modelling of Social Networks
+# Topic: Additive and Multiplicative Effects Model
+# Daniel A. Seussler Becerra
+
+library(tidyverse)
+library(amen)
+library(statnet)
+library(xergm.common)
+
+# Data Description -------------------------------------------------------------
+
+# allyNet Network objects, undirected, three vertex attributes
+#         cinc composite natonal capability, polity score, year
+
+# contigMat indicates shared border
+
+# lnet 
+
+# LSP Matrix recording the number of shared partners 
+
+# warNet whether two states were in a militarized interstate dispute in this year.
+
+
+# Data Exploration -------------------------------------------------------------
+data("alliances")
+
+allyNet["2000"]
+
+
+
+
+
+
+
+
+
+
+
+# Visualization ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+# AMEN Analysis ----------------------------------------------------------------
