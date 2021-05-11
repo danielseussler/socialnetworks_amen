167adb0770f0fafb0619aea5728643d3db8ed428
danielseussler <daniel.seussler@gmail.com>
2021-05-11 21:48:48 +0200

    Restructuring + model work

---
+++ code/socialnetworks_alliances_figures.R
@@ -0,0 +1,96 @@
+# Seminar on Statistical Modeling of Social Networks
+# Code for the Figures
+
+library(amen)
+library(countrycode)
+
+set.seed(42)
+
+# Network Data of Year 2000 ----------------------------------------------------
+data("alliances")
+
+allyNet1981 <- allyNet[["1981"]]
+allyNet2000 <- allyNet[["2000"]]
+lNet2000 <- lNet[["2000"]]
+LSP2000 <- LSP[["2000"]]
+warNet2000 <- warNet[["2000"]]
+
+rm("allyNet", "lNet", "LSP", "warNet")
+
+countries <- colnames(contigMat)
+
+names <- countrycode(countries,
+                     origin = "cowc", destination = "country.name",
+                     custom_match = c("GFR" = "German Federal Republic")
+)
+
+regions <- countrycode(countries,
+                       origin = "cowc", destination = "continent",
+                       custom_match = c(
+                         "CZE" = "Europe", "GDR" = "Europe", "GFR" = "Europe",
+                         "YAR" = "Asia", "YPR" = "Asia", "YUG" = "Europe"
+                       )
+)
+
+set.vertex.attribute(allyNet2000, "region", regions)
+
+
+
+# Introduction: Network Statistics ---------------------------------------------
+is.network(allyNet2000)
+
+summary(allyNet2000, print.adj = FALSE)
+InDegree <- degree(allyNet2000, cmode = "indegree")
+OutDegree <- degree(allyNet2000, cmode = "outdegree") # same as InDegree as sym
+network.size(allyNet2000)
+network.edgecount(allyNet2000)
+network.dyadcount(allyNet2000) # which is n*(n-1)/2
+countries[InDegree > 0]
+gden(allyNet2000, mode = "digraph")
+components(allyNet2000, connected = "strong")
+
+network_statistics <- c(network.size(allyNet2000), network.edgecount(allyNet2000), 
+                        network.dyadcount(allyNet2000), gden(allyNet2000, mode = "digraph"))
+names(network_statistics) <- c("Size", "Edgecount", "Dyadcount", "Density")
+
+hist(InDegree, xlab = "Indegree", main = "In-Degree Distribution", prob = FALSE, breaks = 50)
+hist(InDegree, xlab = "", ylab = "", main = "", prob = FALSE, breaks = 50) #save as degree_histogram.pdf
+countries[InDegree == 23]
+
+saveRDS(network_statistics, file = "analysis/data/socialnetworks_statistics.rds")
+
+
+
+## Set Colors for all Plots ----------------------------------------------------
+col <- character(length = 164)
+col[regions == "Europe"] <- brewer.pal(5, "Blues")[1]
+col[regions == "Asia"] <- brewer.pal(5, "Blues")[2]
+col[regions == "Oceania"] <- brewer.pal(5, "Blues")[3]
+col[regions == "Africa"] <- brewer.pal(5, "Blues")[4]
+col[regions == "Americas"] <- brewer.pal(5, "Blues")[5]
+
+
+
+# Figure 1: The Alliances Data Set ---------------------------------------------
+Y <- as.matrix.network(allyNet2000)
+
+netplot(Y, xlab = "The Alliances Network Year 2000", seed = 42, directed = FALSE, plotnames = FALSE, plot.iso = FALSE)
+netplot(Y, xlab = "The Alliances Network Year 2000", seed = 42, directed = FALSE, plotnames = TRUE)
+netplot(Y, seed = 42, directed = FALSE, plotnames = TRUE, plot.iso = FALSE) #saved as Rplot1.pdf
+netplot(warNet2000, seed = 42)
+netplot(contigMat, seed = 42)
+
+circplot(Y, jitter = 10)    # later for latent variable analysis
+
+Y2 <- Y
+colnames(Y2) <- names; rownames(Y2) <- names
+netplot(Y2, seed = 42, directed = FALSE, plotnames = TRUE, plot.iso = FALSE)
+
+
+
+# Figure 2: The Alliances Data Set - Regional Color Coding ---------------------
+netplot(Y, seed = 42, directed = FALSE, plotnames = FALSE, plot.iso = FALSE, ncol = regions ) #saved as Rplot2.pdf
+
+
+
+# Figure 3: Circplot -----------------------------------------------------------
---
+++ code/socialnetworks_alliances_models.R
@@ -0,0 +1,166 @@
+# Seminar on Statistical Modelling of Social Networks
+# Topic: The Additive and Multiplicative Effects Network Model
+# Daniel A. Seussler Becerra
+
+library(amen)
+library(statnet)
+library(xergm.common)
+library(tictoc)
+
+
+
+# Data Preparation Year 2000 ---------------------------------------------------
+data("alliances")
+
+allyNet2000 <- allyNet[["2000"]]
+lNet2000 <- lNet[["2000"]]
+LSP2000 <- LSP[["2000"]]
+warNet2000 <- warNet[["2000"]]
+countries <- colnames(contigMat)
+
+Y <- as.matrix.network(allyNet2000)
+
+
+
+# ANOVA Decomposition ----------------------------------------------------------
+Rowcountry <- matrix(rownames(Y), nrow(Y), ncol(Y))
+Colcountry <- t(Rowcountry)
+
+anova(lm(c(Y) ~ c(Rowcountry) + c(Colcountry)))
+
+  # Check Model Assumptions Indicates a large degree of heterogeneity, more as if a_i or b_i were all zero.
+
+
+
+# ---- comparison of countries in terms of row and column means (will be the same)
+rmean <- rowMeans(Y, na.rm = TRUE)
+cmean <- colMeans(Y, na.rm = TRUE)
+muhat <- mean(Y, na.rm = TRUE)
+ahat <- rmean - muhat
+bhat <- cmean - muhat
+
+# additive "exporter" effects and importer effects
+head(sort(ahat, decreasing = TRUE))
+head(sort(bhat, decreasing = TRUE)) # unsurprisingly the same as before
+
+# covariance and correlation between row and column effects - same as we have dyadic data
+cov(cbind(ahat, bhat))
+cor(ahat, bhat)
+
+# estimate of dyadic covariance and correlation, does it make sense here?
+R <- Y - (muhat + outer(ahat, bhat, "+"))
+cov(cbind(c(R), c(t(R))), use = "complete")
+cor(c(R), c(t(R)), use = "complete")
+
+
+
+# The Social Relations Model ---------------------------------------------------
+fit_SRM <- ame(Y, family = "bin", symmetric = TRUE)
+saveRDS(fit_SRM, file = "analysis/models/SRMmodel.rds")
+
+fit_SRM <- readRDS(file = "analysis/models/SRMmodel.rds")
+summary(fit_SRM)
+
+
+# Reduced Model that lacks SRM terms of column effects
+fit_SRG <- ame(Y, family = "bin", symmetric = TRUE, rvar = FALSE, cvar = FALSE, dcor = FALSE)
+saveRDS(fit_SRG, file = "analysis/models/SRGmodel.rds")
+
+fit_SRG <- readRDS(file = "analysis/models/SRGmodel.rds")
+summary(fit_SRG)
+
+
+# Define Nodal Covariates
+list.vertex.attributes(allyNet2000)
+Xno <- cbind(get.vertex.attribute(allyNet2000, "year"),
+             get.vertex.attribute(allyNet2000, "cinc"),
+             get.vertex.attribute(allyNet2000, "polity"),
+             get.vertex.attribute(allyNet2000, "na"))
+rownames(Xno) <- get.vertex.attribute(allyNet2000, "vertex.names")
+colnames(Xno) <- c("Year", "cinc", "polity", "na")
+
+
+# Model with added Nodal Covariates Cinc and Polity
+fit_SRRM_nodal <- ame(Y, Xrow = Xno[, 2:3], Xcol = Xno[, 2:3], family = "bin", symmetric = TRUE)
+saveRDS(fit_SRRM_nodal, file = "analysis/models/fit_SRRM_nodal.rds")
+
+fit_SRRM_nodal <- readRDS(file = "analysis/models/fit_SRRM_nodal.rds")
+summary(fit_SRRM_nodal)
+
+
+# Define Dyadic Covariates
+Xdyad <- array(data = c(contigMat, lNet2000, LSP2000, warNet2000),
+               dim = c(164,164,4),
+               dimnames = list(get.vertex.attribute(allyNet2000, "vertex.names"),
+                               get.vertex.attribute(allyNet2000, "vertex.names"),
+                               c("Shared Border","Ally last Year", "Shared Partners before", 
+                                 "War Indicator")))
+
+# Model with added Nodal Covariates Cinc and Polity + All Dyad
+fit_SRRM_nodal_dayd <- ame(Y, Xdyad = Xdyad, Xrow = Xno[, 2:3], Xcol = Xno[, 2:3], family = "bin", symmetric = TRUE)
+saveRDS(fit_SRRM_nodal_dayd, file = "analysis/models/fit_SRRM_nodal_dayd.rds")
+
+fit_SRRM_nodal <- readRDS(file = "analysis/models/fit_SRRM_nodal.rds")
+summary(fit_SRRM_nodal)
+
+
+
+
+# Full AME Model ---------------------------------------------------------------
+fit_AME_R3 <- ame(Y, Xdyad = Xdyad, Xrow = Xno[, 2:3], R = 3, Xcol = Xno[, 2:3], family = "bin", symmetric = TRUE)
+saveRDS(fit_AME_R3, file = "analysis/models/fit_AME_R3.rds")
+
+fit_AME_R3 <- readRDS(file = "analysis/models/fit_AME_R3.rds")
+summary(fit_AME_R3)
+
+
+
+
+
+# Comparison of R - Dimension of Latent Factors --------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+# Time Series Data -------------------------------------------------------------
+year <- c("1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1990", 
+          "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000")
+Y_time <- array(data = 0, dim = c(164, 164, 19), dimnames = list(countries, countries, year))
+XNode_time <- array(data = 0, dim = c(164, 2, 19), dimnames = list(countries, c("cinc", "polity"), year))
+XDyad_time <- array(data = 0, dim = c(164, 164, 4, 19), dimnames = list(countries, countries, c("contigMat", "lNet", "LSP", "warNet"), year))
+
+for (yr in seq_along(year)) {
+  # Sociomatrix
+  year_char <- year[yr]
+  abc <- as.matrix.network(allyNet[[year_char]])
+  Y_time[, , yr] <- abc
+
+  # Nodal Covariates
+  XNode_time[, , yr] <- cbind(
+    get.vertex.attribute(allyNet[[year_char]], "cinc"),
+    get.vertex.attribute(allyNet[[year_char]], "polity")
+  )
+
+  # Dyadic Covariates
+  XDyad_time[, , , yr] <- array(data = c(contigMat, lNet[[year_char]], LSP[[year_char]], warNet[[year_char]]),
+                                dim = c(164, 164, 4))
+}
+
+
+# AME model replicated relational data -----------------------------------------
+tic("Largest Model so far:")
+fit_AME_Rep_R3 <- ame_rep(Y_time, Xdyad = XDyad_time, Xrow = XNode_time, Xcol = XNode_time,
+                          R= 2, family = "bin", symmetric = TRUE)
+toc()
+saveRDS(fit_AME_Rep_R3, file = "analysis/models/fit_AME_Rep_R3.rds")
+
+fit_AME_Rep_R3 <- readRDS(file = "analysis/models/fit_AME_Rep_R3.rds")
+summary(fit_AME_Rep_R3)
