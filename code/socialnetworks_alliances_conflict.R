1ab48d9fc61a7edd3a5e4b85f8a63efb9439628a
danielseussler <daniel.seussler@gmail.com>
2021-06-04 17:36:13 +0200

    update confl func

--- code/socialnetworks_alliances_conflict.R
+++ code/socialnetworks_alliances_conflict.R
@@ -1,18 +1,31 @@
-# Seminar of Statistical Modeling of Social Networks
-# Help: Conflict Indicator - 1 if militarized dispute in the past 10Yrs
+# Seminar on Statistical Modelling of Social Networks
+# Topic: The Additive and Multiplicative Effects Network Model
+# Create conflict indicator
 
 library(xergm.common)
+library(network)
+
+
+# Data Setup
 data("alliances")
+countrycowc <- get.vertex.attribute(allyNet[["2000"]], "vertex.names")
 
 year <- c("1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1990", 
           "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000")
 
-conflict <- matrix(0, nrow = 164, ncol = 164)
- 
+former <- c("YAR", "YPR", "GFR", "GDR", "CZE")
+current <- !(countrycowc %in% former)
+
+
+# Conflict Indicator
+conflict <- matrix(0, nrow = sum(current), ncol = sum(current))
+
 for (yr in year[10:19]){
-  conflict = conflict + data.matrix(warNet[[yr]])
+  conflict = conflict + data.matrix(warNet[[yr]][current, current])
 }
+
+table(conflict)
 
 conflict <- ifelse(conflict > 0, 1, 0)
 
-saveRDS(conflict, file = "data/Conflict.rds")
+saveRDS(conflict, file = "data/conflict.rds")
