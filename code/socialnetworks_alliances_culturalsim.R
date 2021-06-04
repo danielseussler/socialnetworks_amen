9243f7caf388ca7cc1f3392cd5d72c6e66b3ca52
danielseussler <daniel.seussler@gmail.com>
2021-06-04 18:04:22 +0200

    update func culturalsim

--- code/socialnetworks_alliances_culturalsim.R
+++ code/socialnetworks_alliances_culturalsim.R
@@ -1,37 +1,47 @@
-# Seminar of Statistical Modeling of Social Networks
-# Helper Function to determine GeoDistance between two countries, by capital.
+# Seminar on Statistical Modelling of Social Networks
+# Topic: The Additive and Multiplicative Effects Network Model
+# Cultural Similarity: Joint Language Indicator
 # Data from the replication files of Warren 2010
 
-# Issue: Data from 2000 seems to be an identiy matrix, det = 1. 
+# Data from 2000 doesnt seem to be coherent. Identity Matrix?
 # I take data from 1985.
 
-library(xergm.common)
 library(countrycode)
+library(network)
+library(xergm.common)
 
 # Load Data --------------------------------------------------------------------
+jointlanguage <- read.delim("data/raw/networks/joint language 1985.txt", header = FALSE, row.names = NULL)
+jointlanguage <- as.matrix(jointlanguage)
+jointlanguageCountries <- read.delim("data/raw/Country List (1950 - 2000).txt")
+
+rownames(jointlanguage) <- colnames(jointlanguage) <- jointlanguageCountries$cowcode
+
+
+# Selection
 data("alliances")
 
-jointlanguage <- read.delim("data/raw/networks/joint language 1985.txt", header = FALSE, row.names = NULL)
-country <- read.delim("data/raw/Country List (1950 - 2000).txt")
-
-rownames(jointlanguage) <- country$cowcode
-colnames(jointlanguage) <- country$cowcode
-
-alliances <- data.frame("Index" = 1:164, "cowc" = colnames(contigMat))
-alliances$ccode <- countrycode(alliances$cowc,
+countrycowc <- get.vertex.attribute(allyNet[["2000"]], "vertex.names")
+countrycown <- countrycode(countrycowc,
   origin = "cowc", destination = "cown",
   custom_match = c("GFR" = 260)
 )
 
-# Country Selection ------------------------------------------------------------
-ind <- c(country$cowcode) %in% c(alliances$ccode)
-jointlanguage <- jointlanguage[ind, ind]
-CulturalSim <- matrix(data = 0, nrow = 164, ncol = 164, dimnames = list(c(alliances$ccode), c(alliances$ccode)))
-CulturalSim <- jointlanguage
+former <- c("YAR", "YPR", "GFR", "GDR", "CZE")
+formerIndex <- match(former, countrycowc)
+current <- !(countrycowc %in% former)
 
-all(colnames(jointlanguage) == colnames(CulturalSim))
+
+
+# Country Selection ------------------------------------------------------------
+selection <- match(jointlanguageCountries$cowcode, countrycown[current], nomatch = 0)
+
+CulturalSim <- matrix(data = 0, nrow = sum(current), ncol = sum(current), 
+                      dimnames = list(countrycowc[current], countrycowc[current]))
+CulturalSim <- jointlanguage[selection, selection]
+
 det(data.matrix(CulturalSim))
 
-colnames(CulturalSim) <- colnames(contigMat)
-rownames(CulturalSim) <- rownames(contigMat)
-saveRDS(CulturalSim, file = "data/CulturalSim.rds")
+colnames(CulturalSim) <- rownames(CulturalSim) <- countrycowc[current]
+
+saveRDS(CulturalSim, file = "data/culturalsim.rds")
